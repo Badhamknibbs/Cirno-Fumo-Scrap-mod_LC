15913155a8641dd623ebaa91235c686880a2c341
Badhamknibbs <aydenman@hotmail.com>
2023-11-27 21:24:12 +1100

    Change to using LethalLib

--- LethalCompanyTemplate/Plugin.cs
+++ LethalCompanyTemplate/Plugin.cs
@@ -2,77 +2,50 @@
 using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
 
 namespace CirnoFumoScrap
 {
    [BepInPlugin(modGUID, modName, modVersion)]
-   [BepInDependency(LC_API.MyPluginInfo.PLUGIN_GUID)]
+   [BepInDependency(LethalLib.Plugin.ModGUID)]
    public class FumoCirnoMod : BaseUnityPlugin {
       private const string modGUID = "badham.lc.cirnoscrap";
       private const string modName = "Cirno Fumo Scrap";
-      private const string modVersion = "1.0.1";
+      private const string modVersion = "1.2.0";
 
       private readonly Harmony harmony = new Harmony(modGUID);
 
       private static FumoCirnoMod Instance;
 
       private ConfigEntry<int> configFumoRarity;
-      private ConfigEntry<bool> configOopsAllFumo;
 
       internal ManualLogSource mls;
-   
+
+      public static AssetBundle CirnoAssets;
+
       private void Awake() {
          if (Instance == null) {
             Instance = this;
          }
 
-         configFumoRarity = Config.Bind("General", "FumoRarity", 30, new ConfigDescription("How rare Fumo Cirnos are to find. Lower values mean Fumos spawn less often", new AcceptableValueRange<int>(0, 100)));
-         configOopsAllFumo = Config.Bind("Fun", "OopsAllFumo", false, "If enabled, all scrap spawns are replaced with Fumos. May not work as intended with other mods that tweak currentLevel.spawnableScrap");
+         CirnoAssets = AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "fumocirno"));
 
          mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
          mls.LogInfo("Cirno has infiltrated your facilities...");
 
          harmony.PatchAll(typeof(FumoCirnoMod));
-         harmony.PatchAll(typeof(RoundManagerPatch));
-      }
 
-      [HarmonyPatch(typeof(RoundManager))]
-      internal class RoundManagerPatch {
-         [HarmonyPatch("SpawnScrapInLevel")]
-         [HarmonyPrefix]
-         static void FumoInjection(ref SelectableLevel ___currentLevel) {
-            SpawnableItemWithRarity itemWithRarity = new SpawnableItemWithRarity();
-            itemWithRarity.rarity = Instance.configFumoRarity.Value;
-            Item Cirno = LC_API.BundleAPI.BundleLoader.GetLoadedAsset<Item>("assets/fumocirno.asset");
-            if (Cirno == null) {
-               Instance.mls.LogInfo("Failed to find Cirno asset.");
-               return;
-            }
-            itemWithRarity.spawnableItem = Cirno;
-            if (Instance.configOopsAllFumo.Value) {
-               OopsAllFumos(ref ___currentLevel, ref itemWithRarity);
-            } else {
-               AddFumoScrap(ref ___currentLevel, ref itemWithRarity);
-            }
+         configFumoRarity = Config.Bind("General", "FumoRarity", 30, new ConfigDescription("How rare Fumo Cirnos are to find. Lower values mean Fumos spawn less often", new AcceptableValueRange<int>(0, 100)));
+
+         Item Cirno = CirnoAssets.LoadAsset<Item>("assets/fumocirno.asset");
+         if (Cirno == null) {
+            mls.LogError("Failed to load Cirno prefab.");
+         } else {
+            LethalLib.Modules.NetworkPrefabs.RegisterNetworkPrefab(Cirno.spawnPrefab);
+            LethalLib.Modules.Items.RegisterScrap(Cirno, configFumoRarity.Value, LethalLib.Modules.Levels.LevelTypes.All);
          }
-      }
-
-      // Adds fumos to the list of potential scrap spawns
-      static void AddFumoScrap(ref SelectableLevel ___currentLevel, ref SpawnableItemWithRarity itemWithRarity) {
-         Instance.mls.LogInfo("Adding Cirno Fumo as scrap to current level...");
-
-         ___currentLevel.spawnableScrap.Add(itemWithRarity);
-         Instance.mls.LogInfo($"Added Cirno as spawnable scrap with rarity {itemWithRarity.rarity}");
-      }
-
-      // Replace all spawnables with cirno fumos
-      static void OopsAllFumos(ref SelectableLevel ___currentLevel, ref SpawnableItemWithRarity itemWithRarity) {
-         Instance.mls.LogInfo("Replacing spawnable items with Cirno fumos...");
-
-         ___currentLevel.spawnableScrap.Clear();
-         ___currentLevel.spawnableScrap.Add(itemWithRarity);
-         Instance.mls.LogInfo("Successfully set Cirno as only spawnable:");
-         Instance.mls.LogInfo(___currentLevel.spawnableScrap);
       }
    }
 }
