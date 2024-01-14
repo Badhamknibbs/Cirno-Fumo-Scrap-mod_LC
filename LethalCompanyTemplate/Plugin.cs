e3a07324dc50ebf208ac3245acbb2189ac649c10
Badhamknibbs <aydenman@hotmail.com>
2024-01-14 16:44:55 +1100

    v49 fix/update

--- LethalCompanyTemplate/Plugin.cs
+++ LethalCompanyTemplate/Plugin.cs
@@ -13,7 +13,7 @@ namespace CirnoFumoScrap
    public class FumoCirnoMod : BaseUnityPlugin {
       private const string modGUID = "badham.lc.cirnoscrap";
       private const string modName = "Cirno Fumo Scrap";
-      private const string modVersion = "1.2.0";
+      private const string modVersion = "2.0.1";
 
       private readonly Harmony harmony = new Harmony(modGUID);
 
@@ -33,13 +33,13 @@ namespace CirnoFumoScrap
          CirnoAssets = AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "fumocirno"));
 
          mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
-         mls.LogInfo("Cirno has infiltrated your facilities...");
+         mls.LogInfo($"Cirno has infiltrated your facilities [Version {modVersion}]...");
 
          harmony.PatchAll(typeof(FumoCirnoMod));
 
          configFumoRarity = Config.Bind("General", "FumoRarity", 30, new ConfigDescription("How rare Fumo Cirnos are to find. Lower values mean Fumos spawn less often", new AcceptableValueRange<int>(0, 100)));
 
-         Item Cirno = CirnoAssets.LoadAsset<Item>("assets/fumocirno.asset");
+         Item Cirno = CirnoAssets.LoadAsset<Item>("assets/Mods/CirnoFumo/FumoCirno.asset");
          if (Cirno == null) {
             mls.LogError("Failed to load Cirno prefab.");
          } else {
